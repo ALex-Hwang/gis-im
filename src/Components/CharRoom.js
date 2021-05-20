58b4e66139bf4d5de475c1e1c5e4899116016e91
ALex-Hwang <3180102081@zju.edu.cn>
2021-05-21 00:15:13 +0800

    backup

--- src/Components/CharRoom.js
+++ src/Components/CharRoom.js
@@ -1,36 +1,83 @@
 import im from '../config.js'
-import { Input } from 'antd'
-import React, {Component} from 'react'
+import { Button, Input } from 'antd'
+import React, { Component } from 'react'
 
 const { Search } = Input;
+const { TextArea } = Input;
 
-const onSearch = value => console.log(value);
 
-/*
-// Goeasy
-var user = {
-    id:'user000',  //当前用户的唯一标识符
-    data:'{"nickname":"Neo"}' //当前用户的扩展数据, 任意格式的字符串或者对象，用于会话列表conversation.data和群消息的senderData
-}
-
-//连接GoEasy
-var promise =  im.connect(user);
-
-promise.then(function() {
-    console.log("Connection successful.");
-}).catch(function(error) {
-    console.log("Failed to connect GoEasy, code:"+error.code+ ",error:"+error.content);
-});
-*/
 class ChatRoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {User: '', Receiver: '', Message: ''}
+    };
+
+    onSearch = (value) => {
+        console.log(value);
+    };
+    
+    login = (value) =>  {
+        // construct the user info
+        this.setState({User: value})
+        var user = {
+            id: value,  //当前用户的唯一标识符
+            data:'{"nickname":"whatsoever"}' //当前用户的扩展数据, 任意格式的字符串或者对象，用于会话列表conversation.data和群消息的senderData
+        }
+        // connect to Goeasy
+        var promise =  im.connect(user);
+
+        promise.then(function() {
+            console.log("Connection successful. And the userID is: "+value);
+        }).catch(function(error) {
+            console.log("Failed to connect GoEasy, code:"+error.code+ ",error:"+error.content);
+        });
+    };
+
+    // the button to send
+    setSender = (value) => {
+        console.log("the receiver is: " + value);
+        this.setState({Receiver: value});
+    };
+
+    // textArea onChange
+    // about the message to send
+    onChange = (e) => {
+        this.setState({Message: e.target.value});
+    }
+
+    // to send the message
+    // onClick on Button
+    onClick = () => {
+        console.log("user: "+this.state.User)
+        console.log("Receiver: "+this.state.Receiver)
+        console.log("Message: "+this.state.Message)
+    }
+
     render() {
         return (
-            <Search
-            placeholder="input your user id"
-            enterButton="Login"
-            size="large"
-            onSearch={onSearch}
-            />
+            <div>
+                <Search
+                placeholder="input your user id"
+                enterButton="Login"
+                size="large"
+                onSearch={this.login}
+                />
+                <Search
+                placeholder="choose who you wanna send to"
+                enterButton="Confirm"
+                size="large"
+                onSearch={this.setSender}
+                />
+
+                <TextArea 
+                showCount maxLength={100} 
+                onChange={this.onChange} 
+                allowClear={true} 
+                />
+                
+                <Button type="primary" onClick={this.onClick}>Send</Button>
+            </div>
+
         )
     }
 }
